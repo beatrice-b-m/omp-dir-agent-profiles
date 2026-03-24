288340f6570b80af4fe1faecaa3a3374c6258b7e
beatrice-b-m <bbrownmulry@gmail.com>
2026-03-23 23:16:11 -0400

    scaffold: pyproject.toml, package init, .gitignore

---
+++ dir_agent_profile/__init__.py
@@ -0,0 +1,4 @@
+"""Directory-level model role profile manager for Oh My Pi."""
+
+# The five model roles that OMP recognises, in assignment order.
+ROLES: tuple = ("default", "plan", "smol", "slow", "commit")
